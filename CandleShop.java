d36c3fefca0dc3cede1ec4b3fe6673c3be0bde89
Harold Arriola Nucamendi <hnucamend@hnucamendi.attlocal.net>
2022-10-29 02:32:12 -0400

    working on remake

--- CandleShop.java
+++ CandleShop.java
@@ -1,184 +1,116 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
+
+import java.text.DecimalFormat;
 
 // in interaction tab -- > run CandleShop
-import java.util.Scanner;
-import java.text.DecimalFormat;
+
 //The candle names are
 //Luscious Lavender
 //Fragrent Florals
 //Spicy N' Dicey
 
-//List out the Type attributes for candle one
-class TypeCandleOne {
-    public final String TYPE_CANDLE_NAME_1;
-    private double typeCandlePrice1;
-    private int typeCandleBurnTime1;
-    private int typeCandleAmount1;
+class Candle {
+    private String name;
+    private double price;
+    private int burnTime;
+    private int amount;
+    private double totalPrice;
+    private int totalBurnTime;
+    private double costPerBurnTime;
 
-    public TypeCandleOne(String lusciousLavender, double lusciousLavenderPrice, int lusciousLavenderBurnTime,
-            int lusciousLavenderAmount) {
-        this.TYPE_CANDLE_NAME_1 = lusciousLavender;
-        this.typeCandlePrice1 = lusciousLavenderPrice;
-        this.typeCandleBurnTime1 = lusciousLavenderBurnTime;
-        this.typeCandleAmount1 = lusciousLavenderAmount;
+    public Candle(String name, double price, int burnTime, int amount) {
+        this.name = name;
+        this.price = price;
+        this.burnTime = burnTime;
+        this.amount = amount;
     }
 
-    // getters for type one candle, I am making this to make getting and setting
-    // easier for math part later
-    public int lusciousLavenderAmount() {
-        return this.typeCandleAmount1;
+    // Getters
+
+    public String getName() {
+        return this.name;
     }
 
-    public double lusciousLavenderPrice() {
-        return this.typeCandlePrice1;
+    public double getPrice() {
+        return this.price;
     }
 
-    public String lusciousLavender() {
-        return this.TYPE_CANDLE_NAME_1;
+    public int getBurnTime() {
+        return this.burnTime;
     }
 
-    public int lusciousLavenderBurnTime() {
-        return this.typeCandleBurnTime1;
+    public int getAmount() {
+        return this.amount;
     }
 
-    public void setLusciousLavenderAmount(int newLusciousLavenderAmount) {
-        this.typeCandleAmount1 = newLusciousLavenderAmount;
+    // Setters
+
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setLusciousLavenderPrice(double newLusciousLavenderPrice) {
-        this.typeCandlePrice1 = newLusciousLavenderPrice;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
-    public void setLusciousLavenderBurnTime(int newLusciousLavenderBurnTime) {
-        this.typeCandleBurnTime1 = newLusciousLavenderBurnTime;
-    }
-}
-
-// List out the Type attributes for candle two
-class TypeCandleTwo {
-    public final String TYPE_CANDLE_NAME_2;
-    private double typeCandlePrice2;
-    private int typeCandleBurnTime2;
-    private int typeCandleAmount2;
-
-    public TypeCandleTwo(String fragrantFlorals, double fragrantFloralsPrice, int fragrantFloralsBurnTime,
-            int fragrantFloralsAmount) {
-        this.TYPE_CANDLE_NAME_2 = fragrantFlorals;
-        this.typeCandlePrice2 = fragrantFloralsPrice;
-        this.typeCandleBurnTime2 = fragrantFloralsBurnTime;
-        this.typeCandleAmount2 = fragrantFloralsAmount;
+    public void setBurnTime(int burnTime) {
+        this.burnTime = burnTime;
     }
 
-    public int fragrantFloralsAmount() {
-        return this.typeCandleAmount2;
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 
-    public double fragrantFloralsPrice() {
-        return this.typeCandlePrice2;
+    // Logic
+
+    public double totalPrice() {
+        this.totalPrice = (double) this.amount * this.price;
+        return this.totalPrice;
     }
 
-    public String fragrantFlorals() {
-        return this.TYPE_CANDLE_NAME_2;
+    public int totalBurnTime() {
+        this.totalBurnTime = this.amount * this.burnTime;
+        return this.totalBurnTime;
     }
 
-    public int fragrantFloralsBurnTime() {
-        return this.typeCandleBurnTime2;
+    public double costPerBurnTime() {
+        this.costPerBurnTime = this.totalPrice() / this.burnTime;
+        return costPerBurnTime;
     }
 
-    public void setFragrantFloralsAmount(int newFragrantFloralsAmount) {
-        this.typeCandleAmount2 = newFragrantFloralsAmount;
-    }
-
-    public void setFragrantFloralsPrice(double newFragrantFloralsPrice) {
-        this.typeCandlePrice2 = newFragrantFloralsPrice;
-    }
-
-    public void setFragrantFloralsBurnTime(int newFragrantFloralsBurnTime) {
-        this.typeCandleBurnTime2 = newFragrantFloralsBurnTime;
-    }
-}
-
-// List out the Type attributes for candle three
-class TypeCandleThree {
-    public final String TYPE_CANDLE_NAME_3;
-    private double typeCandlePrice3;
-    private int typeCandleBurnTime3;
-    private int typeCandleAmount3;
-
-    public TypeCandleThree(String spicyNDicey, double spicyNDiceyPrice, int spicyNDiceyBurnTime,
-            int spicyNDiceyAmount) {
-        this.TYPE_CANDLE_NAME_3 = spicyNDicey;
-        this.typeCandlePrice3 = spicyNDiceyPrice;
-        this.typeCandleBurnTime3 = spicyNDiceyBurnTime;
-        this.typeCandleAmount3 = spicyNDiceyAmount;
-    }
-
-    public int spicyNDiceyAmount() {
-        return this.typeCandleAmount3;
-    }
-
-    public double spicyNDiceyPrice() {
-        return this.typeCandlePrice3;
-    }
-
-    public String spicyNDicey() {
-        return this.TYPE_CANDLE_NAME_3;
-    }
-
-    public int spicyNDiceyBurnTime() {
-        return this.typeCandleBurnTime3;
-    }
-
-    public void setSpicyNDiceyAmount(int newSpicyNDiceyAmount) {
-        this.typeCandleAmount3 = newSpicyNDiceyAmount;
-    }
-
-    public void setSpicyNDicey(double newSpicyNDiceyPrice) {
-        this.typeCandlePrice3 = newSpicyNDiceyPrice;
-    }
-
-    public void setSpicyNDiceyBurnTime(int newSpicyNDiceyBurnTime) {
-        this.typeCandleBurnTime3 = newSpicyNDiceyBurnTime;
-    }
 }
 
 public class CandleShop {
     public static void main(String[] args) throws InterruptedException {
+        double grandTotal;
+        int grandTotalBurnTime;
+        double costPerBurnTime;
+
         // Candle Objects
-        TypeCandleOne candleOne = new TypeCandleOne("Luscious Lavender", 2.50, 5, 0);
-        TypeCandleTwo candleTwo = new TypeCandleTwo("Fragrant Floral's", 3.75, 7, 0);
-        TypeCandleThree candleThree = new TypeCandleThree("Spicy N' Dicey", 5.99, 12, 0);
+        Candle candleOne = new Candle("Luscious Lavender", 2.50, 5, 0);
+        Candle candleTwo = new Candle("Fragrant Floral's", 3.75, 7, 0);
+        Candle candleThree = new Candle("Spicy N' Dicey", 5.99, 12, 0);
+
+        Candle[] candles = new Candle[] { candleOne, candleTwo, candleThree };
 
         Scanner keyboard = new Scanner(System.in);
         // other variables to help complete the algorithm to sell Candles
         System.out.println("Enter 0 to continue: ");
-        int continuePrompt = keyboard.nextInt();
-        System.out.println(
-                "How many of the " + candleOne.lusciousLavender() + " candles would you like? Enter a number amount.");
-        int lusciousLavenderKey = keyboard.nextInt();
-        System.out.println("How many of the Fragrant Floral's candles would you like? Enter a number amount.");
-        int fragrantFloralsKey = keyboard.nextInt();
-        System.out.println("How many of the Spice N' Dice candles would you like? Enter a number amount.");
-        int spicyNDiceyKey = keyboard.nextInt();
-        // Set the keyboard inputs for amounts
-        candleOne.setLusciousLavenderAmount((int) lusciousLavenderKey);
-        candleTwo.setFragrantFloralsAmount((int) fragrantFloralsKey);
-        candleThree.setSpicyNDiceyAmount((int) spicyNDiceyKey);
 
-        // gets the price of each candle individually
-        double userCandleChoicePrice1 = (double) candleOne.lusciousLavenderAmount() * candleOne.lusciousLavenderPrice();
-        double userCandleChoicePrice2 = (double) candleTwo.fragrantFloralsAmount() * candleTwo.fragrantFloralsPrice();
-        double userCandleChoicePrice3 = (double) candleThree.spicyNDiceyAmount() * candleThree.spicyNDiceyPrice();
-        // adds up the total of each individual candle
-        double userCandleTotalPrice = (double) userCandleChoicePrice1 + userCandleChoicePrice2 + userCandleChoicePrice3;
-        // Determines the total Burn time in hours
-        int totalBurnTime = (int) (lusciousLavenderKey * candleOne.lusciousLavenderBurnTime())
-                + (fragrantFloralsKey * candleTwo.fragrantFloralsBurnTime())
-                + (spicyNDiceyKey * candleThree.spicyNDiceyBurnTime());
-        // Determines the amount per burn time of each candle
-        double updatedBurnTime1 = (double) userCandleChoicePrice1 / candleOne.lusciousLavenderBurnTime();
-        double updatedBurnTime2 = (double) userCandleChoicePrice2 / candleTwo.fragrantFloralsBurnTime();
-        double updatedBurnTime3 = (double) userCandleChoicePrice3 / candleThree.spicyNDiceyBurnTime();
-        double updatedBurnTimeTotal = (double) updatedBurnTime1 + updatedBurnTime2 + updatedBurnTime3;
+        for (int i = 0; i < candles.length; i++) {
+            keyboard.nextInt();
+            System.out.println(String.format(
+                    "How many of the %s candles would you like? Enter a number amount.", candles[i].getName()));
+            candles[i].setAmount(keyboard.nextInt());
+
+            candles[i].totalPrice();
+
+            grandTotal += candles[i].totalPrice();
+            grandTotalBurnTime += candles[i].totalBurnTime();
+            costPerBurnTime += candles[i].costPerBurnTime();
+        }
 
         // I used two methods to format my double values
         // This is where I format using an import text method
